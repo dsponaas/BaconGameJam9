1e4483e8efd705c4714eb5328d96054931bfcf3a
sponezzis <dsponaas@gmail.com>
2015-06-12 18:26:13 -0400

    initial commit

---
+++ android/src/com/grizbenzis/bgj9/android/AndroidLauncher.java
@@ -0,0 +1,16 @@
+package com.grizbenzis.bgj9.android;
+
+import android.os.Bundle;
+
+import com.badlogic.gdx.backends.android.AndroidApplication;
+import com.badlogic.gdx.backends.android.AndroidApplicationConfiguration;
+import com.grizbenzis.bgj9.bgj9;
+
+public class AndroidLauncher extends AndroidApplication {
+	@Override
+	protected void onCreate (Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		AndroidApplicationConfiguration config = new AndroidApplicationConfiguration();
+		initialize(new bgj9(), config);
+	}
+}
---
+++ desktop/src/com/grizbenzis/bgj9/desktop/DesktopLauncher.java
@@ -0,0 +1,12 @@
+package com.grizbenzis.bgj9.desktop;
+
+import com.badlogic.gdx.backends.lwjgl.LwjglApplication;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.grizbenzis.bgj9.bgj9;
+
+public class DesktopLauncher {
+	public static void main (String[] arg) {
+		LwjglApplicationConfiguration config = new LwjglApplicationConfiguration();
+		new LwjglApplication(new bgj9(), config);
+	}
+}
